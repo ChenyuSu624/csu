3d3fab48af79b6a3bbc569607ba7f8d873b98667
Chenyu Su <csu@csumb.edu>
2018-03-03 08:21:18 +0000

    lab3

--- Labs/lab3/Sliver_jack.php
+++ Labs/lab3/Sliver_jack.php
@@ -1,34 +1,43 @@
 <?php
+class Card {
+	public static $usedCards = array();
+	public $suit = 0;
+	public $val = 0;
 
-function displayCards(){
-    
-    $deck = array();
-    $suits = array("clubs","spades","hearts","diamonds");
-    
-    for ($i = 0; $i < 4; $i++) {
-        for ($j = 1; $j <= 13; $j++) {
-            $deck[] = $suits[$i] . "_" . $j;
-        }
-    }
-    
-    shuffle($deck);
-    
-    foreach ($deck as $card) {
-        echo $card . "<br />";
-    }
+	public static $suits = array("clubs", "diamonds", "hearts", "spades");
 
+	public function getPath() {
+		return "cards/" . Card::$suits[$this->suit] . "/" . ($this->val + 1) . ".png";
+	}
+
+	public static function initUsedCards() {
+		for ($i = 0; $i < 4; $i++) {
+			Cards::$usedCards[i] = array();
+			for ($j = 0; $j < 13; $j++)
+				Cards::$usedCards[i][j] = false;
+		}
+	}
+
+	function __construct() {
+		do {
+			$this->suit = rand(0, 3);
+			$this->val = rand(0, 12);
+		} while (Card::$usedCards[$this->suit][$this->val]);
+		Card::$usedCards[$this->suit][$this->val] = true;
+	}
 }
-?>
 
+$c = new Card();
+echo $c->getPath(), PHP_EOL;
+
+?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title> 
-        
-        </title>
+        <title> </title>
     </head>
     <body>
-        <?=displayCards()?>
+        <? 
     </body>
 </html>
