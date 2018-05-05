243e98391ecb5f2c689daf8eb2cd42242c5fe262
Chenyu Su <csu@csumb.edu>
2018-05-05 22:09:00 +0000

    lab10

---
+++ Labs/lab10/about.php
@@ -0,0 +1,23 @@
+<?php
+
+    include 'inc/header.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About</title>
+    </head>
+    <body>
+        <p><strong>Chenyu Su</strong></p>
+        <p>CST 336 Internet programming class</p>
+        <P>Californnia State University of Monterey Bay</P>
+    </body>
+</html>
+
+<?php
+
+    include 'inc/footer.php';
+
+?>
